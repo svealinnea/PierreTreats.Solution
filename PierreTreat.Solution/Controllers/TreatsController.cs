278c6baad2c2598b997f0c8ea6f29ed130c167ae
Svea Wade <svealinneawade@gmail.com>
2021-01-15 10:45:51 -0800

    adds ViewModels and Authorize

--- PierreTreat.Solution/Controllers/TreatsController.cs
+++ PierreTreat.Solution/Controllers/TreatsController.cs
@@ -3,9 +3,15 @@ using PierreTreat.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System;
 namespace PierreTreat.Controllers
 {
+  [Authorize]
   public class TreatsController : Controller 
   {
     private readonly PierreTreatContext _db; 
